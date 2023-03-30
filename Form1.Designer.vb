69d7b5c61f9ff20fd73392cc5fe677cd93a2b04d
davidane <54445028+davidane@users.noreply.github.com>
2023-03-30 13:03:40 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.leftButton = New System.Windows.Forms.Button()
+        Me.downButton = New System.Windows.Forms.Button()
+        Me.rightButton = New System.Windows.Forms.Button()
+        Me.upButton = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'leftButton
+        '
+        Me.leftButton.Location = New System.Drawing.Point(97, 63)
+        Me.leftButton.Name = "leftButton"
+        Me.leftButton.Size = New System.Drawing.Size(75, 23)
+        Me.leftButton.TabIndex = 0
+        Me.leftButton.Text = "Left"
+        Me.leftButton.UseVisualStyleBackColor = True
+        '
+        'downButton
+        '
+        Me.downButton.Location = New System.Drawing.Point(171, 92)
+        Me.downButton.Name = "downButton"
+        Me.downButton.Size = New System.Drawing.Size(75, 23)
+        Me.downButton.TabIndex = 1
+        Me.downButton.Text = "Down"
+        Me.downButton.UseVisualStyleBackColor = True
+        '
+        'rightButton
+        '
+        Me.rightButton.Location = New System.Drawing.Point(243, 63)
+        Me.rightButton.Name = "rightButton"
+        Me.rightButton.Size = New System.Drawing.Size(75, 23)
+        Me.rightButton.TabIndex = 2
+        Me.rightButton.Text = "Right"
+        Me.rightButton.UseVisualStyleBackColor = True
+        '
+        'upButton
+        '
+        Me.upButton.Location = New System.Drawing.Point(171, 34)
+        Me.upButton.Name = "upButton"
+        Me.upButton.Size = New System.Drawing.Size(75, 23)
+        Me.upButton.TabIndex = 3
+        Me.upButton.Text = "Up"
+        Me.upButton.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 46)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.Text = "Off"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 69)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(53, 17)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.Text = "Direct"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(12, 92)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(68, 17)
+        Me.RadioButton3.TabIndex = 6
+        Me.RadioButton3.Text = "Stabalize"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Mode"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.upButton)
+        Me.Controls.Add(Me.rightButton)
+        Me.Controls.Add(Me.downButton)
+        Me.Controls.Add(Me.leftButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents leftButton As Button
+    Friend WithEvents downButton As Button
+    Friend WithEvents rightButton As Button
+    Friend WithEvents upButton As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,279 @@
+﻿Public Class Form1
+    Dim all18Bytes(17) As Byte
+    Dim directModeBool, stabalizeModeBool, offModeBool As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SerialPort1.PortName = "COM5" 'name serial port
+        SerialPort1.BaudRate = 57600  'set baud rate 19.2k
+        SerialPort1.DataBits = 8 'number of data bits is 8
+        SerialPort1.StopBits = IO.Ports.StopBits.One 'one stop bit
+        SerialPort1.Parity = IO.Ports.Parity.None 'no parity bits
+        SerialPort1.Open() 'intialize and open port
+        Timer1.Enabled = True 'enable timer 1 on form
+
+        RadioButton1.Checked = True
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+
+        directionalButtonsOff()
+
+        offModeEnabled()
+
+
+        all18Bytes(0) = &H55
+        all18Bytes(1) = &HAA
+        all18Bytes(2) = &H7
+        all18Bytes(3) = &H4D
+        all18Bytes(4) = &H0
+        all18Bytes(5) = &H1
+        all18Bytes(6) = &H0
+        all18Bytes(7) = &H98
+        all18Bytes(8) = &H0
+        all18Bytes(8) = &H0
+        all18Bytes(10) = &H0
+        all18Bytes(11) = &H0
+        all18Bytes(12) = &H0
+        all18Bytes(13) = &H0
+        all18Bytes(14) = &H0
+        all18Bytes(15) = &H0
+        all18Bytes(16) = &H0
+        all18Bytes(17) = &H0
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked = True Then
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+
+            offModeEnabled()
+        End If
+
+        upButton.Enabled = False
+        downButton.Enabled = False
+        leftButton.Enabled = False
+        rightButton.Enabled = False
+    End Sub
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked = True Then
+            RadioButton1.Checked = False
+            RadioButton3.Checked = False
+
+            directModeEnabled()
+        End If
+
+        directionalButtonsOn()
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        If RadioButton3.Checked = True Then
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+
+            stabalizeModeEnabled()
+        End If
+
+        directionalButtonsOn()
+    End Sub
+
+    Private Sub directionalButtonsOn()
+        upButton.Enabled = True
+        downButton.Enabled = True
+        leftButton.Enabled = True
+        rightButton.Enabled = True
+    End Sub
+
+    Private Sub directionalButtonsOff()
+        upButton.Enabled = False
+        downButton.Enabled = False
+        leftButton.Enabled = False
+        rightButton.Enabled = False
+    End Sub
+
+    Private Sub offModeEnabled()
+        offModeBool = True
+        directModeBool = False
+        stabalizeModeBool = False
+        all18Bytes(10) = &H0
+    End Sub
+
+    Private Sub directModeEnabled()
+        offModeBool = False
+        directModeBool = True
+        stabalizeModeBool = False
+        'all18Bytes(10) = &H10
+    End Sub
+
+    Private Sub stabalizeModeEnabled()
+        offModeBool = False
+        directModeBool = False
+        stabalizeModeBool = True
+        'all18Bytes(10) = &H20
+    End Sub
+
+    Private Sub leftButton_Click(sender As Object, e As EventArgs) Handles leftButton.Click
+        controlGimbalByte9()
+
+        If directModeBool = True Then
+            directBytes10()
+            directLeftBytes1617()
+        ElseIf stabalizeModeBool = True Then
+            stabalizeBytes10()
+            stabalizeLeftBytes1617()
+        End If
+
+        controlGimbalBytes11()
+        LeftBytes1213()
+        LeftRightBytes1415()
+
+        SerialPort1.Write(all18Bytes, 0, 18)
+    End Sub
+
+    Private Sub rightButton_Click(sender As Object, e As EventArgs) Handles rightButton.Click
+        controlGimbalByte9()
+
+        If directModeBool = True Then
+            directBytes10()
+            directRightBytes1617()
+        ElseIf stabalizeModeBool = True Then
+            stabalizeBytes10()
+            stabalizeRightBytes1617()
+        End If
+
+        controlGimbalBytes11()
+        RightBytes1213()
+        LeftRightBytes1415()
+
+        SerialPort1.Write(all18Bytes, 0, 18)
+    End Sub
+
+    Private Sub upButton_Click(sender As Object, e As EventArgs) Handles upButton.Click
+        controlGimbalByte9()
+
+        If directModeBool = True Then
+            directBytes10()
+            directUpBytes16()
+            directUpDownBytes17()
+        ElseIf stabalizeModeBool = True Then
+            stabalizeBytes10()
+            stabalizeUpBytes16()
+            stabalizeUpDownBytes17()
+        End If
+        controlGimbalBytes11()
+        upDownBytes1213()
+        upBytes1415()
+
+
+        SerialPort1.Write(all18Bytes, 0, 18)
+    End Sub
+
+    Private Sub downButton_Click(sender As Object, e As EventArgs) Handles downButton.Click
+        controlGimbalByte9()
+
+        If directModeBool = True Then
+            directBytes10()
+            directDownBytes16()
+            directUpDownBytes17()
+        ElseIf stabalizeModeBool = True Then
+            stabalizeBytes10()
+            stabalizeDownBytes16()
+            stabalizeUpDownBytes17()
+        End If
+        controlGimbalBytes11()
+        upDownBytes1213()
+        downBytes1415()
+
+        SerialPort1.Write(all18Bytes, 0, 18)
+    End Sub
+
+    Private Sub controlGimbalByte9()
+        all18Bytes(9) = &H47
+    End Sub
+
+    Private Sub directBytes10()
+        all18Bytes(10) = &H10
+    End Sub
+
+    Private Sub stabalizeBytes10()
+        all18Bytes(10) = &H20
+    End Sub
+
+    Private Sub controlGimbalBytes11()
+        all18Bytes(11) = &H0
+    End Sub
+
+    Private Sub LeftBytes1213()
+        all18Bytes(12) = &H97
+        all18Bytes(13) = &H7C
+    End Sub
+
+    Private Sub RightBytes1213()
+        all18Bytes(12) = &H69
+        all18Bytes(13) = &H83
+    End Sub
+
+    Private Sub LeftRightBytes1415()
+        all18Bytes(14) = &H0
+        all18Bytes(15) = &H80
+    End Sub
+
+    Private Sub directLeftBytes1617()
+        all18Bytes(16) = &H2F
+        all18Bytes(17) = &HFB
+    End Sub
+
+    Private Sub stabalizeLeftBytes1617()
+        all18Bytes(16) = &HDF
+        all18Bytes(17) = &HFE
+    End Sub
+
+    Private Sub directRightBytes1617()
+        all18Bytes(16) = &HF7
+        all18Bytes(17) = &HFA
+    End Sub
+
+    Private Sub stabalizeRightBytes1617()
+        all18Bytes(16) = &H7
+        all18Bytes(17) = &HFF
+    End Sub
+
+    Private Sub upDownBytes1213()
+        all18Bytes(12) = &H0
+        all18Bytes(13) = &H80
+    End Sub
+
+    Private Sub upBytes1415()
+        all18Bytes(14) = &H97
+        all18Bytes(15) = &H7C
+    End Sub
+
+    Private Sub directUpBytes16()
+        all18Bytes(16) = &H1A
+    End Sub
+
+    Private Sub stabalizeUpBytes16()
+        all18Bytes(16) = &HEA
+    End Sub
+
+    Private Sub downBytes1415()
+        all18Bytes(14) = &H69
+        all18Bytes(15) = &H83
+    End Sub
+
+    Private Sub directDownBytes16()
+        all18Bytes(16) = &H3A
+    End Sub
+
+    Private Sub stabalizeDownBytes16()
+        all18Bytes(16) = &HCA
+    End Sub
+
+    Private Sub directUpDownBytes17()
+        all18Bytes(17) = &H79
+    End Sub
+
+    Private Sub stabalizeUpDownBytes17()
+        all18Bytes(17) = &H7C
+    End Sub
+
+End Class
